6007b58b912d97d8cf3e956781fce1e3798eac6e
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-10 17:20:17 -0300

    Match functionality

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,33 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
 //import AsyncStorage from '@react-native-community/async-storage';
+
 import {
   View,
   Text,
   SafeAreaView,
   Image,
   StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+  TouchableOpacity
+} from "react-native";
 
-import api from '../services/api';
+import api from "../services/api";
 
-import logo from '../assets/logo.png';
-import dislike from '../assets/dislike.png';
-import like from '../assets/like.png';
+import logo from "../assets/logo.png";
+import dislike from "../assets/dislike.png";
+import like from "../assets/like.png";
+import itsamatch from "../assets/itsamatch.png";
 
 export default function Main({ navigation }) {
-  const id = navigation.getParam("user");
+  const id = navigation.getParam('user');
   const [users, setUsers] = useState([]); //Initializing users
+  const [matchDev, setMatchDev] = useState(null);
 
   //console.log("Id do usuário", id);
 
   useEffect(() => {
     //Load users from api
     async function loadUsers() {
-      const response = await api.get('/devs', {
+      const response = await api.get("/devs", {
         headers: {
-          user: id,
-        }
+          user: id
+        },
       });
 
       setUsers(response.data);
@@ -36,10 +41,31 @@ export default function Main({ navigation }) {
     loadUsers();
   }, [id]);
 
+  useEffect(() => {
+    // Connect with backend about Socket to Match funcionality
+    const socket = io('http://localhost:3333', {
+      query: { user: id }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    });
+
+    /*socket.on('world', message => {
+      console.log(message)
+    })
+
+    setTimeout(() => {
+      socket.emit('hello', {
+        message: 'Hello World'
+      })
+    },3000)*/
+  }, [id]);
+
   async function handleLike() {
     const [user, ...rest] = users; // Get user to be liked
     await api.post(`/devs/${user._id}/likes`, null, {
-      headers: { user: id },
+      headers: { user: id }
     });
 
     setUsers(rest); // Rebuilding the user list
@@ -48,7 +74,7 @@ export default function Main({ navigation }) {
   async function handleDisLike() {
     const [user, ...rest] = users; // Get user to be disliked
     await api.post(`/devs/${user._id}/dislikes`, null, {
-      headers: { user: id },
+      headers: { user: id }
     });
 
     setUsers(rest); // Rebuilding the user list
@@ -56,7 +82,7 @@ export default function Main({ navigation }) {
 
   async function handleLogout() {
     //await AsyngStorage.clear();
-    navigation.navigate('Login');
+    navigation.navigate("Login");
   }
 
   return (
@@ -77,7 +103,7 @@ export default function Main({ navigation }) {
               <Image
                 style={styles.avatar}
                 source={{
-                  uri: user.avatar,
+                  uri: user.avatar
                 }}
               />
               <View style={styles.footer}>
@@ -101,6 +127,23 @@ export default function Main({ navigation }) {
           </TouchableOpacity>
         </View>
       )}
+
+      {matchDev && (
+        <View style={styles.matchContainer}>
+          <Image style={styles.matchImage} source={itsamatch} />
+          <Image
+            style={styles.matchAvatar}
+            source={{
+              uri: matchDev.avatar
+            }}
+          />
+          <Text style={styles.matchName}>{matchDev.name}</Text>
+          <Text style={styles.matchBio}>{matchDev.bio}</Text>
+          <TouchableOpacity onPress={() => setMatchDev(null)}>
+            <Text style={styles.closeMatch}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -108,86 +151,130 @@ export default function Main({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
-    alignItems: "center",
-    justifyContent: "space-between",
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
 
   logo: {
-    marginTop: 30
+    marginTop: 30,
   },
 
   empty: {
-    alignSelf: 'center',
-    color: '#999',
+    alignSelf: "center",
+    color: "#999",
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
 
   cardsContainer: {
     flex: 1,
-    alignSelf: "stretch",
-    justifyContent: "center",
-    maxHeight: 500
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
   },
 
   card: {
     borderWidth: 1,
-    borderColor: "#ddd",
+    borderColor: '#ddd',
     borderRadius: 8,
     margin: 30,
-    overflow: "hidden",
-    position: "absolute",
+    overflow: 'hidden',
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0
+    bottom: 0,
   },
 
   avatar: {
     flex: 1,
-    height: 300
+    height: 300,
   },
 
   footer: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingHorizontal: 20,
-    paddingVertical: 15,
+    paddingVertical: 15
   },
 
   name: {
     fontSize: 16,
-    fontWeight: "bold",
-    color: '#333',
+    fontWeight: 'bold',
+    color: "#333"
   },
 
   bio: {
     fontSize: 14,
-    color: '#999',
+    color: "#999",
     marginTop: 5,
-    lineHeight: 18
+    lineHeight: 18,
   },
 
   buttonsContainer: {
-    flexDirection: "row",
-    marginBottom: 30
+    flexDirection: 'row',
+    marginBottom: 30,
   },
 
   button: {
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
     marginHorizontal: 20,
     elevation: 2,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOpacity: 0.05,
     shadowRadius: 2,
     shadowOffset: {
       width: 0,
-      height: 2,
-    },
+      height: 2
+    }
+  },
+
+  matchContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  matchImage: {
+    height: 80,
+    resizeMode: "contain",
+  },
+
+  matchAvatar: {
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+    borderWidth: 5,
+    borderColor: '#fff',
+    marginVertical: 30
+  },
+
+  matchName: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+
+  matchBio: {
+    marginTop: 10,
+    fontSize: 16,
+    color: "rgba(255, 255, 255, 0.8)",
+    lineHeight: 24,
+    textAlign: 'center',
+    paddingHorizontal: 30
+  },
+
+  closeMatch: {
+    fontSize: 16,
+    color: "rgba(255, 255, 255, 0.8)",
+    textAlign: 'center',
+    marginTop: 30,
+    fontWeight: "bold",
   },
 });
